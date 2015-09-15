1d0f45daefeb46cf579289cac4de86ff1058cb06
Sean Holcomb <seanholcomb@gmail.com>
2015-09-15 11:24:23 -0700

    Implement Java library for suppying jokes

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -1,11 +1,14 @@
 package com.udacity.gradle.builditbigger;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
+import com.example.Joker;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 
@@ -22,6 +25,16 @@ public class MainActivityFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_main, container, false);
+
+        Button jokeButton = (Button) root.findViewById(R.id.joke_button);
+        jokeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Joker jokester = new Joker();
+                Toast.makeText(getActivity(), jokester.getJoke(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
 
         AdView mAdView = (AdView) root.findViewById(R.id.adView);
         // Create an ad request. Check logcat output for the hashed device ID to
---
+++ jokelib/src/main/java/com/example/Joker.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import java.lang.String;
+
+public class Joker {
+    public Joker(){
+    }
+    public String getJoke(){
+        return "Why did the scarecrow get promoted? He was outstanding in his field";
+    }
+
+
+}
