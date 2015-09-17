a3800d8559b42b896269dea79155426f64e05559
Sean Holcomb <seanholcomb@gmail.com>
2015-09-16 18:49:51 -0700

    added androidTest for AsyncTask

---
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/AsyncTestCase.java
@@ -0,0 +1,23 @@
+package com.udacity.gradle.builditbigger;
+
+import android.test.AndroidTestCase;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by seanholcomb on 9/16/15.
+ *
+ */
+public class AsyncTestCase extends AndroidTestCase {
+    public void verifyNonemptyString() {
+        String joke = "";
+        try {
+            EndpointsAsyncTask jokeTask = new EndpointsAsyncTask();
+            jokeTask.execute(getContext());
+            joke = jokeTask.get(30, TimeUnit.SECONDS);
+        } catch (Exception e) {
+            fail("Timed out");
+        }
+        assertEquals(joke.equals(""), false);
+    }
+}
